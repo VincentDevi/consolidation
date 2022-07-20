33113524304d6414d69846896c347bb0f8919c57
Devilers Vincent <vincent.devilers@gmail.com>
2022-07-20 20:20:44 +0200

    a little bit of scss and class are named in the JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -22,7 +22,7 @@ let movieList = [
     },
     bond = {
         "film_poster" : "assets/picture/bond.jpg",
-        "tilte" : "No Time To die",
+        "title" : "No Time To die",
         "genre" : "Action",
         "release_date" : "30 september 2021",
         "description" :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -42,30 +42,40 @@ let movieList = [
         "film_poster" : "assets/picture/alice.jpg",
         "title" : "Still Alice",
         "genre" : "Drama",
-        "realease_date" : "5 december 2014",
+        "release_date" : "5 december 2014",
         "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "critics" :"4/5",
         "trailer" : "un lien"
     },
     gump = {
-        film_poster : "assets/picture/gump.jpg",
-        title : "Forrest Gump",
-        genre : "Comedy",
-        release_date : "6 july 1994",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        critics:"5/5",
+        "film_poster" : "assets/picture/gump.jpg",
+        "title" : "Forrest Gump",
+        "genre" : "Comedy",
+        "release_date" : "6 july 1994",
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "critics":"5/5",
         "trailer" : "un lien"
+    },
+    club = {
+        "film_poster" : "assets/picture/club.jpg",
+        "title" : "Fight Club",
+        "genre" : "Action",
+        "release_date": "10 november 1999",
+        "description": "une description",
+        "critics": "5/5",
+        "trailer": "un lien"
+    },
+    pulp = {
+        "film_poster" : "assests/picture/pulp.jpg",
+        "title" : "Pulp Fiction",
+        "genre" : "Gangster",
+        "release_date": "9 november 1994",
+        "description": "une description",
+        "critics": "5/5",
+        "trailer": "un lien"
     }
-];
 
-    let newArticle = document.createElement("article");
-    let newimg = document.createElement("img");
-    let newH3 = document.createElement("h3");
-    let newDiv = document.createElement("div");
-    let newH2 = document.createElement("h2");
-    let newPara = document.createElement("p");
-    let newA = document.createElement("a");
-    let newh4 = document.createElement("h4");
+];
     /*
     let listElement = ["article", "img", "h3", "h4", "div", "h2", "p", "a"];
     for (let i = 0; i < listElement.length; i++){
@@ -99,6 +109,7 @@ function createSection(anObject) {
 
 function cards(movie) {
     let ok = document.querySelector("main");
+    ok.className = "movie";
     let sec = document.createElement("section");
     ok.appendChild(sec);
     let im = document.createElement("img");
@@ -111,17 +122,21 @@ function cards(movie) {
 
     let divi = document.createElement("article");
     sec.appendChild(divi);
+    divi.className = "movie__information";
 
         let titre = document.createElement("h2");
         titre.appendChild(document.createTextNode(movie["title"]));
+        titre.className = "movie__information__titre"
         divi.appendChild(titre);
 
         let date = document.createElement("p");
         date.appendChild(document.createTextNode(movie["release_date"]));
+        date.className = "movie__information__date"
         divi.appendChild(date);
 
         let desc = document.createElement("p");
-        date.appendChild(document.createTextNode(movie["description"]));
+        desc.appendChild(document.createTextNode(movie["description"]));
+        desc.className = "movie__information__description"
         divi.appendChild(desc);
 
         let note = document.createElement("h4");
@@ -130,6 +145,7 @@ function cards(movie) {
 
     let link = document.createElement("a");
     link.appendChild(document.createTextNode(movie["trailer"]));
+    link.className = "movie__link"
     sec.appendChild(link);
 
 }
