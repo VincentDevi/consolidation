0389bd448ad21ce207b08fd109c38298d4c6da4f
Devilers Vincent <vincent.devilers@gmail.com>
2022-07-25 17:05:32 +0200

    need to add a trailer, last navbar and responsive for nav

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,7 +17,7 @@ let movieList = [
         "title" : "Top Gun",
         "genre" : "Action",
         "release_date" : "25 mai 2022",
-        "description" :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "description" :"After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.",
         "critics" :"4/5",
         "trailer" : "un lien"
     },
@@ -26,25 +26,25 @@ let movieList = [
         "title" : "No Time To die",
         "genre" : "Action",
         "release_date" : "30 september 2021",
-        "description" :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "description" :"Bond has left active service and is enjoying a tranquil life in Jamaica. His peace is short-lived when his old friend Felix Leiter from the CIA turns up asking for help. The mission to rescue a kidnapped scientist turns out to be far more treacherous than expected, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.",
         "critics" :"3/5",
-        "trailer" : "un lien"
+        "trailer" : "Trailer"
     },
     conjuring ={
         "film_poster": "assets/picture/conjuring.jpg",
         "title" : "Conjuring",
         "genre" : "Horror",
         "release_date" : "21 august 2013",
-        "description" :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "description" :"Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse.",
         "critics" :"4/5",
-        "trailer" : "un lien"
+        "trailer" : "link"
     },
     alice = {
         "film_poster" : "assets/picture/alice.jpg",
         "title" : "Still Alice",
         "genre" : "Drama",
         "release_date" : "5 december 2014",
-        "description" : "Alice Howland, professeure de linguistique à l'Université de Columbia, fête son cinquantième anniversaire avec son mari médecin John et leurs trois enfants adultes. Après qu'elle a oublié un mot pendant une conférence et s'est perdue pendant un jogging sur le campus, le médecin d'Alice lui diagnostique un début de maladie d'Alzheimer congénitale. Deux de ses enfants, Anna et Tom, se font dépister. Lydia, sa fille cadette et actrice débutante, décide de ne pas savoir",
+        "description" : "Alice Howland is a renowned linguistics professor happily married with three grown children. All that begins to change when she strangely starts to forget words and then more. When her doctor diagnoses her with Early-onset Alzheimer's Disease, Alice and her family's lives face a harrowing challenge as this terminal degenerative neurological ailment slowly progresses to an inevitable conclusion they all dread. Along the way, Alice struggles to not only to fight the inner decay, but to make the most of her remaining time to find the love and peace to make simply living worthwhile",
         "critics" :"4/5",
         "trailer" : "un lien"
     },
@@ -53,7 +53,7 @@ let movieList = [
         "title" : "Forrest Gump",
         "genre" : "Comedy",
         "release_date" : "6 july 1994",
-        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "description" : "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
         "critics":"5/5",
         "trailer" : "un lien"
     },
@@ -62,7 +62,7 @@ let movieList = [
         "title" : "Fight Club",
         "genre" : "Action",
         "release_date": "10 november 1999",
-        "description": "une description",
+        "description": "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
         "critics": "5/5",
         "trailer": "un lien"
     },
@@ -74,6 +74,33 @@ let movieList = [
         "description": "Pulp Fiction's narrative is told out of chronological order, and follows three main interrelated stories that each have a different protagonist: Vincent Vega, a hitman; Butch Coolidge, a prizefighter; and Jules Winnfield, Vincent's business partner. ",
         "critics": "5/5",
         "trailer": "un lien"
+    },
+    peur = {
+        "film_poster" : "assets/picture/peur.jpg",
+        "title" : "La Cité de la peur",
+        "genre" : "Comedy",
+        "release_date" : "9 march  1994",
+        "description" : "Odile Deray, a press agent, comes to the Cannes Film Festival to present the film 'Red is Dead'. Unfortunately, the film is so weak that no one wants to hear about it. However, when the projectionists of the film in question die each in turn in strange circumstances, `Red is Dead' benefits from an incredible publicity. Serge Karamazov is then charged with protecting the new projectionist of the film.",
+        "critics": "5/5",
+        "trailer" : "a link" 
+    },
+    park = {
+        "film_poster" : "assets/picture/park.jpg",
+        "title" : "Jurassic Park",
+        "genre" : "Action",
+        "release_date" : "20 october 1993",
+        "description" : "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
+        "critics" : "4/5",
+        "trailer" : "a link"
+    },
+    ghost = {
+        "film_poster" : "assets/picture/ghost1.jpg",
+        "title" : "Ghost Buster",
+        "genre" : "Fantasy",
+        "release_date" : "8 june 1984",
+        "description" : "Three parapsychologists forced out of their university funding set up shop as a unique ghost removal service in New York City, attracting frightened yet skeptical customers.",
+        "critics" : "5/5",
+        "trailer" : " a link"
     }
 
 ];
